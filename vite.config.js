16f15eaf4037ec987ca6205b27b44fcbe6127add
unknown <mahdiswatt@gmail>
2025-03-11 05:47:46 +0330

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/defs/",
   plugins: [
     react(),
   ],
+  base: "/defs/",
   server: {
     proxy: {
       '/api': {
@@ -16,4 +16,19 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    outDir: "dist",
+    assetsDir: "src",
+    rollupOptions: {
+      output: {
+        entryFileNames: "src/[name].js",
+        chunkFileNames: "src/[name].js",
+        assetFileNames: "src/[name].[ext]",
+      },
+    },
+  },
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
+  }  
+
 });
