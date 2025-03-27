439863ece4cc502685badba2b28930b51cf0c77d
Nima <88092252+Nima-Nilchian@users.noreply.github.com>
2025-03-27 17:47:13 +0330

    Update api name

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Loader from '../components/Loader';
 
-const API_BASE_URL = "https://bandbooster.liara.run";
+const API_BASE_URL = "https://api-bandbooster.liara.run";
 
 const LoadingPage = () => {
   const navigate = useNavigate();
